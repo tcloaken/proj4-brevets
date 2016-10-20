3fceb9d47cdba79722da56de05a6cb3c623b5775
tcloaken <trevor.enright@gmail.com>
2016-10-20 16:25:46 -0700

    added tests

---
+++ test_acp_times.py
@@ -0,0 +1,35 @@
+#! /bin/python3
+"""
+
+nose tests for acp times
+
+"""
+from acp_times import open_time
+from acp_times import close_time
+import arrow
+
+
+def test_open_200():
+    """
+    Is desired open times
+	"""
+    test_start_time = arrow.get('2016-11-21 16:00:00', 'YYYY-MM-DD HH:mm:ss')
+    print (test_start_time)
+    print (open_time(50,200,test_start_time))
+    print (arrow.get('2016-11-21 17:28:00', 'YYYY-MM-DD HH:mm:ss'))
+    assert open_time(50,200,test_start_time) == arrow.get('2016-11-21 17:28:00', 'YYYY-MM-DD HH:mm:ss')
+    
+    
+def test_close_200():
+    """
+    Is desired open times
+	"""
+    test_start_time = arrow.get('2016-11-21 16:00:00', 'YYYY-MM-DD HH:mm:ss')
+    print (test_start_time)
+    print (close_time(50,200,test_start_time))
+    print (arrow.get('2016-11-21 17:28:00', 'YYYY-MM-DD HH:mm:ss'))
+    assert close_time(50,200,test_start_time) == arrow.get('2016-11-21 19:20:00', 'YYYY-MM-DD HH:mm:ss')
+    
+    
+    
+
