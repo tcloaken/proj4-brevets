7a47b5a7f4b9d178bd1eb49fab2ca559bed8fe0b
tcloaken <trevor.enright@gmail.com>
2016-10-20 17:06:36 -0700

    added stuff

--- test_acp_times.py
+++ test_acp_times.py
@@ -8,15 +8,16 @@ from acp_times import open_time
 from acp_times import close_time
 import arrow
 
+def test_fail():
+    test_start_time = arrow.get('2016-11-21 16:00:00', 'YYYY-MM-DD HH:mm:ss')
+    assert open_time(50,200,test_start_time) == arrow.get('2016-11-21 16:00:00', 'YYYY-MM-DD HH:mm:ss')
+    assert close_time(50,200,test_start_time) == arrow.get('2016-11-21 16:00:00', 'YYYY-MM-DD HH:mm:ss')
 
 def test_open_200():
     """
     Is desired open times
 	"""
     test_start_time = arrow.get('2016-11-21 16:00:00', 'YYYY-MM-DD HH:mm:ss')
-    print (test_start_time)
-    print (open_time(50,200,test_start_time))
-    print (arrow.get('2016-11-21 17:28:00', 'YYYY-MM-DD HH:mm:ss'))
     assert open_time(50,200,test_start_time) == arrow.get('2016-11-21 17:28:00', 'YYYY-MM-DD HH:mm:ss')
     
     
@@ -29,7 +30,3 @@ def test_close_200():
     print (close_time(50,200,test_start_time))
     print (arrow.get('2016-11-21 17:28:00', 'YYYY-MM-DD HH:mm:ss'))
     assert close_time(50,200,test_start_time) == arrow.get('2016-11-21 19:20:00', 'YYYY-MM-DD HH:mm:ss')
-    
-    
-    
-
